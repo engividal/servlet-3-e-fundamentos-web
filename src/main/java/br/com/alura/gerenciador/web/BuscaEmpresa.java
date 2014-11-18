65189d6f24987bdb9c31a557dbcfc9586725aa69
Ismael Jr <engividal@gmail.com>
2014-11-18 04:43:57 -0400

    Capitulo 4 - Filtrando Requisições
    Aprendendo a utilizar filtros e movendo o diretorio no so para pasta
    git.

--- src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -13,30 +13,29 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.alura.gerenciador.Empresa;
 import br.com.alura.gerenciador.dao.EmpresaDAO;
 
-@WebServlet("/busca")
+@WebServlet(urlPatterns="/busca")
 public class BuscaEmpresa extends HttpServlet{
 
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		String filtro = req.getParameter("filtro");
 		
-		String filtro = "doce";//req.getParameter("filtro");
 		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
-
-		PrintWriter writer = resp.getWriter();
 		
-		writer.println("<html>");
+		PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
         writer.println("<body>");
         writer.println("Resultado busca:<br/>");
-		
         writer.println("<ul>");
         for(Empresa empresa : empresas) {
-          writer.println("<li>" + empresa.getId() + ": " + empresa.getNome() + "</li>");
-          
+             writer.println("<li>" + empresa.getId() + ": " + empresa.getNome() + "</li>");
         }
         writer.println("</ul>");
-        
         writer.println("</body>");
         writer.println("</html>");
+		
+		
 	}
 }
